d21106455ce145222557287ae8851c2a834d2aed
Nakarin Singhashsathit <nakarin.sing@gmail.com>
2025-10-30 10:21:47 +0700

    Update nexus_core.py

--- nexus_core.py
+++ nexus_core.py
@@ -16,7 +16,7 @@ import seaborn as sns
 import pandas as pd
 from river import datasets, metrics, ensemble, tree, preprocessing
 from river.base import Classifier
-from river.proba import Bernoulli
+from river.dist import Bernoulli
 import json
 from collections import deque
 from tqdm import tqdm
@@ -426,3 +426,4 @@ def main() -> None:
 
 if __name__ == "__main__":
     main()
+
